f37c78adca43d275639d8978f25bb52b7ef07683
Saloni-000 <53488784+narkaraditya10@users.noreply.github.com>
2024-05-18 17:10:28 +0530

    hero section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React from "react";
 
 const Highlights = () => {
-  return (
-    <div>Highlights</div>
-  )
-}
 
-export default Highlights
+  useGSAP(()=>{
+    gsap.to("#title",{})
+  })
+
+  return (
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc"
+    >
+      <div className="screen-max-width">
+        <div className="mb-12 w-full items-end justify-between">
+          <h1 id="title" className="section-heading">
+            The Highlights
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Highlights;
