9c1ffe2ac45658887d74c9c36675c6851c25d402
narkaraditya10 <53488784+narkaraditya10@users.noreply.github.com>
2024-05-28 22:38:42 +0530

    changes

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,12 +1,22 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import React from "react";
+import { watchImg } from "../utils";
+import VideoCarousal from "./VideoCarousal";
 
 const Highlights = () => {
-
-  useGSAP(()=>{
-    gsap.to("#title",{})
-  })
+  useGSAP(() => {
+    gsap.to("#title", {
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".link", {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.25,
+    });
+  }, []);
 
   return (
     <section
@@ -14,11 +24,22 @@ const Highlights = () => {
       className="w-screen overflow-hidden h-full common-padding bg-zinc"
     >
       <div className="screen-max-width">
-        <div className="mb-12 w-full items-end justify-between">
+        <div className="mb-12 w-full md:flex items-end justify-between">
           <h1 id="title" className="section-heading">
-            The Highlights
+            Get The Highlights
           </h1>
+          <div className=" flex  flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={watchImg} alt="watch" className="ml-2" />
+            </p>
+          </div>
         </div>
+        <VideoCarousal />
       </div>
     </section>
   );
